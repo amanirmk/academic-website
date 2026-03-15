001980f4397a3bbdcc67f471dc66f035b59b8c43
amanirmk <amainakilaas@hmc.edu>
2026-03-15 17:05:30 -0400

    minor updates to example publications

--- src/content/profile_info.js
+++ src/content/profile_info.js
@@ -11,7 +11,7 @@ export const profile_pic = headshot;
 // Use <Link> for internal links and <a> for external links.
 
 export const introduction = <div>
-  <p>I recently defended my thesis at No-Federal-Funding University, where I spent my entire time following the direction of a <Link to="/publications#pub-3">first-year project</Link> I randomly picked. My research focuses on things that sound impressive at dinner parties but are actually just me staring at error messages until they go away. I collaborate extensively with my rubber duck and occasionally with humans.</p>
+  <p>I recently defended my thesis at No-Federal-Funding University, where I spent my entire time following the direction of a <Link to="/publications#pub-4">first-year project</Link> I randomly picked. My research focuses on things that sound impressive at dinner parties but are actually just me staring at error messages until they go away. I collaborate extensively with my rubber duck and occasionally with humans.</p>
   <p>My academic journey began when I realized I wasn't ready for the real world and discovered that universities will pay you a small but livable wage to have existential crises in their buildings. Before coming to NFFU, I didn't know what I was doing in college, which gave me the confidence to not know what I'm doing in grad school. I am supported by several <a href="https://en.wikipedia.org/wiki/Fellow">fellowships</a> for some mysterious reason.</p>
 </div>;
 
--- src/content/publication_entries.js
+++ src/content/publication_entries.js
@@ -1,19 +1,34 @@
 const preparation = [
   {
     linkhash: "prep-1",
+    year: 2028,
     title: "After 10 Submissions to NeurIPS, We Gave Up.",
     author_span: <span>Author, <b>John Doe</b>, Author, Author, and Author</span>,
+    paper: "paper1.pdf",
   },
   {
     linkhash: "prep-2",
+    year: 2027,
     title: "Genuinely Amazing Work that Only Exists on ArXiv.",
-    author_span: <span>Author<sup>&dagger;</sup>, <b>John Doe<sup>&dagger;</sup></b>, Author, Author, Author, and Author</span>
+    author_span: <span>Author<sup>&dagger;</sup>, <b>John Doe<sup>&dagger;</sup></b>, Author, Author, Author, and Author</span>,
+    paper: "paper1.pdf",
+    doi: "https://www.doi.org/",
+    code: "https://github.com/",
   },
 ];
 
 const publications = [
   {
     linkhash: "pub-1",
+    year: 2025,
+    title: "Claude Messed Up Our References.",
+    author_span: <span>Author<sup>&dagger;</sup>, <b>John Doe<sup>&dagger;</sup></b>, Author, Author, and Author</span>,
+    location: "13th International Conference on a Very Cool Topic",
+    paper: "paper1.pdf",
+    doi: "https://www.doi.org/",
+  },
+  {
+    linkhash: "pub-2",
     year: 2023,
     title: "On the Limitations of My Career.",
     author_span: <span>Author, <b>John Doe</b>, Author, Author and Author</span>,
@@ -23,7 +38,7 @@ const publications = [
     code: "https://github.com/",
   },
   {
-    linkhash: "pub-2",
+    linkhash: "pub-3",
     year: 2022,
     title: "We Trained a Model and It Worked, We're Not Sure Why Either.",
     author_span: <span><b>John Doe</b>, Author, Author, Author, and Author</span>,
@@ -32,21 +47,14 @@ const publications = [
     doi: "https://www.doi.org/",
   },
   {
-    linkhash: "pub-3",
+    linkhash: "pub-4",
     year: 2021,
     title: "Our Baseline Outperformed Our Method So We Made It Look Worse.",
     author_span: <span><b>John Doe</b>, Author, Author, and Author</span>,
     location: "2021 IEEE Congress on Fancy Computation",
+    paper: "paper2.pdf",
     doi: "https://www.doi.org/",
     code: "https://github.com/",
-  },
-  {
-    linkhash: "pub-4",
-    year: 2020,
-    title: "Claude Messed Up Our References.",
-    author_span: <span>Author<sup>&dagger;</sup>, <b>John Doe<sup>&dagger;</sup></b>, Author, Author, and Author</span>,
-    location: "13th International Conference on a Very Cool Topic",
-    doi: "https://www.doi.org/",
   },
 ];
 
